4832a8a0c0af96d45ebde7b55dec9c24e55a6e94
TimMedin <timmedin180@gmail.com>
2025-03-16 14:24:45 +0200

    upd

---
+++ script.js
@@ -0,0 +1,28 @@
+let age = 15;
+console.log(age);
+
+let name = "Тимофій";
+console.log(name);
+
+let isStudent = true;
+console.log(isStudent);
+
+let myString = "Немає нічого неможливого.";
+console.log(myString);
+
+let myNumber = 50;
+myNumber += 10;
+console.log(myNumber);
+
+let myNull = null;
+console.log(myNull);
+
+let userName = prompt("Введіть своє ім’я");
+alert("Ваше ім’я: " + userName);
+console.log("Тип змінної: ", typeof userName);
+
+let userAnswer = confirm("Ви дійсно хочете покинути сторінку?");
+console.log("Результат вибору:", userAnswer);
+
+alert("Увага! Дія може бути небезпечною.");
+let confirmAction = confirm("Ви впевнені, що хочете продовжити?");
